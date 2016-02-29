0ba09b68600a3c1f0d9e13243f70b5d0dd2d1964
aashish647 <aashish647@missionrnd.com>
2016-02-28 23:56:38 -0800

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,7 +13,7 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
+#include<stdlib.h>
 struct transaction {
 	int amount;
 	char date[11];
@@ -21,5 +21,40 @@ struct transaction {
 };
 
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+
+	if ((Arr == NULL) || (date == NULL))
+	{
+		return NULL;
+	}
+	int count = 0, i = 0, year1, year2, day1, day2, month1, month2;
+	day1 = (date[0] - '\0') * 10 + (date[1] - '\0');
+	month1 = (date[3] - '\0') * 10 + (date[4] - '\0');
+	year1 = (date[6] - '\0') * 1000 + (date[7] - '\0') * 100 + (date[8] - '\0') * 10 + (date[9] - '\0');
+	for (i = 0; i < len; i++)
+	{
+		year2 = (Arr[i].date[6] - '\0') * 1000 + (Arr[i].date[7] - '\0') * 100 + (Arr[i].date[8] - '\0') * 10 + (Arr[i].date[9] - '\0');
+		if (year2 > year1)
+		{
+			count++;
+		}
+		if (year2 == year1)
+		{
+			month2 = (Arr[i].date[3] - '\0') * 10 + (Arr[i].date[4] - '\0');
+			if (month2 > month1)
+			{
+				count++;
+			}
+			if (month2 == month1)
+			{
+				day2 = (Arr[i].date[0] - '\0') * 10 + (Arr[i].date[1] - '\0');
+				if (day2 > day1)
+				{
+					count++;
+				}
+			}
+		}
+	}
+	return count;
 }
+
+
